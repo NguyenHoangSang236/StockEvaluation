25118c1e5d7e74124bd637f1ef862212fca0edc9
NguyenHoangSang236 <nguyenhoangsang236@gmail.com>
2024-07-23 16:59:26 +0700

    update

--- src/main/java/com/stock_trading/evaluation/EvaluationApplication.java
+++ src/main/java/com/stock_trading/evaluation/EvaluationApplication.java
@@ -5,11 +5,13 @@ import com.stock_trading.evaluation.services.DataCrawlingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EvaluationApplication {
 	public static void main(String[] args) throws InterruptedException, URISyntaxException {
 		SpringApplication.run(EvaluationApplication.class, args);
